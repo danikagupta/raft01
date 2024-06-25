1a4c2cb930fdde3a85b41d3a33e3b901c7d8ef6c
danikagupta <74515001+danikagupta@users.noreply.github.com>
2024-06-25 03:22:52 +0000

    Added review node

--- graph.py
+++ graph.py
@@ -12,7 +12,9 @@ class AgentState(TypedDict):
     messages: List[Dict]
     category: str
     sampleResponse: str
+    initialResponse: str
     lastUserRequest: str
+    analysis: str
     responseToUser: str
     lnode: str
 
@@ -22,6 +24,13 @@ class AgentState(TypedDict):
 class Category(BaseModel):
     category: str
     sampleResponse: str
+
+class Response(BaseModel):
+    response: str
+
+class FinalResponse(BaseModel):
+    analysis: str
+    response: str
 
 VALID_CATEGORIES=["Product","Raft","SmallTalk","Abuse","Other"]
 
@@ -38,26 +47,31 @@ class ChatAnswerer():
         builder.add_node("answerRaftQuestions", self.answerRaftQuestions)
         builder.add_node("answerSmallTalk", self.answerSmallTalk)
         builder.add_node("answerAbuse", self.answerAbuse)
+        builder.add_node("reviewResponse", self.reviewResponse)
 
         builder.set_entry_point("initial_classifier")
         builder.add_conditional_edges('initial_classifier',self.main_router,
-                                      {"Other":END, 
+                                      {"Other":"answerSmallTalk", 
                                        "Product":"answerProductQuestions", 
                                        "Raft":"answerRaftQuestions", 
                                        "SmallTalk":"answerSmallTalk",
                                        "Abuse":"answerAbuse",})
-        builder.add_edge("answerProductQuestions", END)
-        builder.add_edge("answerRaftQuestions", END)
-        builder.add_edge("answerSmallTalk", END)
-        builder.add_edge("answerAbuse", END)
+        builder.add_edge("answerProductQuestions", "reviewResponse")
+        builder.add_edge("answerRaftQuestions", "reviewResponse")
+        builder.add_edge("answerSmallTalk", "reviewResponse")
+        builder.add_edge("answerAbuse", "reviewResponse")
+        builder.add_edge("reviewResponse", END)
+        
         memory = SqliteSaver(conn=sqlite3.connect(":memory:", check_same_thread=False))
         self.graph = builder.compile(checkpointer = memory)
-        #self.graph.get_graph().draw_png("graph.png")
+        self.graph.get_graph().draw_png("graph.png")
 
     def initialClassifier(self, state: AgentState):
         print("START: initial_classifier")
         my_prompt=f"""
                 You are getting requests from users aboutr RAFT.
+                Founded in San Jose in 1994, RAFT (Resource Area For Teaching) is a nonprofit providing educators with engaging hands-on 
+        learning resources aligned to national content standards.
                 Please classify the request.
                 
                 If the request is question about a specific product, category is 'Product'
@@ -95,25 +109,119 @@ class ChatAnswerer():
         
     def answerProductQuestions(self, state: AgentState):
         print("START: answerProductQuestions")
-        return {"responseToUser": state['sampleResponse']}
+        question=state['lastUserRequest']
+        sample_response=state['sampleResponse']
+        background_info=""
+        my_prompt=f"""
+                You are getting requests from users about a specific RAFT product.
+                The question is {question}.
+                Here is a sample response {sample_response}.
+                Here is some useful information about the product.
+                {background_info}
+                
+                Please provide a  response.
+                """
+        llm_response=self.model.with_structured_output(Response).invoke([
+            SystemMessage(content=my_prompt),
+            HumanMessage(content=state['lastUserRequest']),
+        ])
+        return {"initialResponse": llm_response.response }
     
     def answerRaftQuestions(self, state: AgentState):
         print("START: answerRaftQuestions")
-        return {"responseToUser": state['sampleResponse']}
+        question=state['lastUserRequest']
+        sample_response=state['sampleResponse']
+        background_info=f"""
+        Founded in San Jose in 1994, RAFT (Resource Area For Teaching) is a nonprofit providing educators with engaging hands-on 
+        learning resources aligned to national content standards.
+
+        RAFT’s mission is to help parents and educators transform a child’s learning experience through hands-on education. 
+        We help bridge the education gap for students and communities by offering engaging STEAM resources to encourage 
+        the next generation of innovators, problem solvers, and creators. RAFT does this by providing STEAM Project Kits, 
+        Maker Mobile van visits and makerspace builds, STEAM summer camps, tailored educator workshops, and free online 
+        learning activity sheets. RAFT’s unique Found Engineering process repurposes donated materials that would have 
+        otherwise been sent to the landfill into accessible, low-cost, STEAM Project Kits and assembled by volunteers. 
+        Educators, and now parents, can become RAFT members and shop at the warehouse store facility for affordable 
+        items devoted to hands-on learning.
+        """
+        my_prompt=f"""
+                You are getting requests from users about RAFT in general.
+                The question is {question}.
+                
+                Here is some useful information about RAFT.
+                {background_info}
+                
+                Please provide a  response.
+                """
+        llm_response=self.model.with_structured_output(Response).invoke([
+            SystemMessage(content=my_prompt),
+            HumanMessage(content=state['lastUserRequest']),
+        ])
+        return {"initialResponse": llm_response.response }
     
     def answerSmallTalk(self, state: AgentState):
         print("START: answerSmallTalk")
-        return {"responseToUser": state['sampleResponse']}
+        question=state['lastUserRequest']
+        sample_response=state['sampleResponse']
+        background_info=""
+        my_prompt=f"""
+                You are getting requests from users.
+                The current ask is {question}.
+                This question was previously classifies as "small-talk".
+                Please respond politely and professionally, but direct the user to questions about RAFT and/or RAFT products.
+                Here is a sample response {sample_response}.
+                
+                Please provide a  response.
+                """
+        llm_response=self.model.with_structured_output(Response).invoke([
+            SystemMessage(content=my_prompt),
+            HumanMessage(content=state['lastUserRequest']),
+        ])
+        return {"initialResponse": llm_response.response }
     
     def answerAbuse(self, state: AgentState):
         print("START: answerAbuse")
-        return {"responseToUser": state['sampleResponse']}
-    
-    def answerOther(self, state: AgentState):
-        print("START: answerOther")
-        return {"responseToUser": state['sampleResponse']}
-    
-    def answerUnknown(self, state: AgentState):
-        print("START: answerUnknown")
-        return {"responseToUser": "I am sorry, I do not understand your request."}
+        question=state['lastUserRequest']
+        sample_response=state['sampleResponse']
+        background_info=""
+        my_prompt=f"""
+                You are getting requests from users.
+                The current ask is {question}.
+                This question was previously classifies as abusive.
+                Please respond politely and professionally, but firmly to dissuade the user from further abusive behavior.
+                Here is a sample response {sample_response}.
+                
+                Please provide a  response.
+                """
+        llm_response=self.model.with_structured_output(Response).invoke([
+            SystemMessage(content=my_prompt),
+            HumanMessage(content=state['lastUserRequest']),
+        ])
+        return {"initialResponse": llm_response.response }
+
+
+    def reviewResponse(self, state: AgentState):
+        print("START: reviewResponse")
+        question=state['lastUserRequest']
+        initial_response=state['initialResponse']
+        category=state['category']
+        my_prompt=f"""
+                You are an expert at reviewing and revising responses.
+                The user's request is :{question}:.
+                This request was classified as :{category}:.
+                The initial draft of response is :{initial_response}:.
+                Please review the response to be professional and courteous.
+                Based on this analysis, please provide the final response to be 
+                shared with the user.
+                
+                Please provide both the analysis and the final response.
+                """
+        llm_response=self.model.with_structured_output(FinalResponse).invoke([
+            SystemMessage(content=my_prompt),
+            HumanMessage(content=state['lastUserRequest']),
+        ])
+        return {
+            "analysis": llm_response.analysis,
+            "responseToUser": llm_response.response,
+              }
     
